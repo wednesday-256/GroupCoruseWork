92728c00b40e31eee4aac30b0c8b4ea0d3e85531
Elhassan Elnasir <elnasir555@yahoo.com>
2023-05-11 16:58:10 +0400

    Adding comments to explain

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+//using an express module
+
 const express = require("express");
+
+//setting up the path
+
 const path = require("path");
+
 const app = express();
+
+//specifying the router
 
 var apiRouter = require("./routes/api_router");
 
