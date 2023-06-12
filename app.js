14456fd68d3eb43d1128500c00c7c791cb2d17d5
Felix Wednesday <felix.dido.4@gmail.com>
2023-06-12 17:54:49 +0400

    added comments

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 //using an express module
-
 const express = require("express");
 
 //setting up the path
-
 const path = require("path");
 
 const app = express();
 
 //specifying the router
-
 var apiRouter = require("./routes/api_router");
 
 app.use("/", apiRouter);
--- routes/api_router.js
+++ routes/api_router.js
@@ -2,17 +2,21 @@ const express = require("express");
 const ALLOWED_IPS = ["127.0.0.1", "123.456.7.89"];
 const api = express.Router();
 
+//lessons details
 const lessons = [
   { topic: "math", location: "Hendon", price: 100 },
   { topic: "math", location: "Colindale", price: 80 },
   { topic: "math", location: "Brent Cross", price: 90 },
   { topic: "math", location: "Golders Green", price: 120 },
 ];
+
+//get request for lessons
 api.get("/lessons", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(lessons);
 });
 
+//users details
 const users = {
   email: "user@email.com",
   password: "myPassword",
